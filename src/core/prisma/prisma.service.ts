c3e42205fbc6c50726411b044134d58e2f3109f2
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2026-06-12 22:02:18 +0300

    feat(prisma): integrate Prisma with NestJS and add configuration files

---
+++ src/core/prisma/prisma.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from 'generated/prisma/client';
+import { ConfigService } from '../config/config.service';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  constructor(private readonly configService: ConfigService) {
+    const adapter = new PrismaPg({
+      connectionString: configService.databaseUrl,
+    });
+    super({ adapter });
+  }
+}
