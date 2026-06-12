4983469f80d3ed75dbb704e4a0080b035cd4d4ce
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2026-06-12 13:05:09 +0300

    feat(config): restructure application configuration and remove unused components

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
