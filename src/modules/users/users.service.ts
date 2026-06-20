3498f41433ef378ffde5a42c731270291422d227
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2026-06-20 12:08:04 +0300

    Revert "refactor(users): simplify findAll method in UsersService and remove unused query parameters"
    This reverts commit a2b09378e5f61488fd86623a3461d35d7a26fc2e.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,24 +1,55 @@
 import { Injectable } from '@nestjs/common';
-import { UserStatus } from 'generated/prisma/client';
+import { UserStatus, Prisma } from 'generated/prisma/client';
 import { PrismaService } from 'src/core/prisma/prisma.service';
 import { BulkIdsDto } from './dto/bulk-ids.dto';
+import { FindUsersQueryDto } from './dto/find-users-query.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  public async findAll() {
-    return await this.prisma.user.findMany({
-      orderBy: { lastLoginAt: 'desc' },
-      select: {
-        id: true,
-        name: true,
-        email: true,
-        status: true,
-        lastLoginAt: true,
-        createdAt: true,
-      },
-    });
+  public async findAll({
+    page,
+    limit,
+    sortBy,
+    sortOrder,
+    search,
+  }: FindUsersQueryDto) {
+    const skip = (page - 1) * limit;
+    const where: Prisma.UserWhereInput = search
+      ? {
+          OR: [
+            { name: { contains: search, mode: 'insensitive' } },
+            { email: { contains: search, mode: 'insensitive' } },
+          ],
+        }
+      : {};
+    const select: Prisma.UserSelect = {
+      id: true,
+      name: true,
+      email: true,
+      status: true,
+      lastLoginAt: true,
+      createdAt: true,
+    };
+
+    const [data, total] = await this.prisma.$transaction([
+      this.prisma.user.findMany({
+        where,
+        skip,
+        take: limit,
+        orderBy: { [sortBy]: sortOrder },
+        select,
+      }),
+      this.prisma.user.count({ where }),
+    ]);
+
+    return {
+      data,
+      total,
+      page,
+      limit,
+    };
   }
 
   public async bulkBlock(bulkIdsDto: BulkIdsDto) {
