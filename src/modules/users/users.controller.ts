a2b09378e5f61488fd86623a3461d35d7a26fc2e
Denis Gavrin <35220917+dgavrin@users.noreply.github.com>
2026-06-20 03:17:42 +0300

    refactor(users): simplify findAll method in UsersService and remove unused query parameters

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,13 +6,11 @@ import {
   HttpCode,
   HttpStatus,
   Post,
-  Query,
   UseGuards,
 } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { BulkIdsDto } from './dto/bulk-ids.dto';
 import { UsersService } from './users.service';
-import { FindUsersQueryDto } from './dto/find-users-query.dto';
 
 @Controller('users')
 @UseGuards(JwtAuthGuard)
@@ -21,8 +19,8 @@ export class UsersController {
 
   @Get()
   @HttpCode(HttpStatus.OK)
-  public async findAll(@Query() query: FindUsersQueryDto) {
-    return await this.usersService.findAll(query);
+  public async findAll() {
+    return await this.usersService.findAll();
   }
 
   @Post('block')
